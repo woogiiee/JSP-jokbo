e4cacdb414b712fe319de3412f864621c52f9e21
woogiiee <65659198+woogiiee@users.noreply.github.com>
2020-06-26 18:50:55 +0900

    datasource

---
+++ src/edu/emp/EmpDTO.java
@@ -0,0 +1,107 @@
+package edu.emp;
+
+public class EmpDTO {
+	private String empno;
+	private String ename;
+	private String job;
+	private String mgr;
+	private String hiredate;
+	private String sal;
+	private String comm;
+	private String deptno;
+	
+	public EmpDTO() {}
+	
+	
+	
+	/**
+	 * @param empno
+	 * @param ename
+	 * @param job
+	 * @param mgr
+	 * @param hiredate
+	 * @param sal
+	 * @param comm
+	 * @param deptno
+	 */
+	public EmpDTO(String empno, String ename, String job, String mgr, String hiredate, String sal, String comm,
+			String deptno) {
+		super();
+		this.empno = empno;
+		this.ename = ename;
+		this.job = job;
+		this.mgr = mgr;
+		this.hiredate = hiredate;
+		this.sal = sal;
+		this.comm = comm;
+		this.deptno = deptno;
+	}
+
+
+
+	public String getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(String empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public String getMgr() {
+		return mgr;
+	}
+
+	public void setMgr(String mgr) {
+		this.mgr = mgr;
+	}
+
+	public String getHiredate() {
+		return hiredate;
+	}
+
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+
+	public String getSal() {
+		return sal;
+	}
+
+	public void setSal(String sal) {
+		this.sal = sal;
+	}
+
+	public String getComm() {
+		return comm;
+	}
+
+	public void setComm(String comm) {
+		this.comm = comm;
+	}
+
+	public String getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(String deptno) {
+		this.deptno = deptno;
+	}
+	
+	
+}
